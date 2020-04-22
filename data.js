e727bf0d3edb9be626a6391ebf866ea0510a60bc
skyproject0 <64170263+skyproject0@users.noreply.github.com>
2020-04-22 23:28:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,86 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-21",
+      "name": "21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.086320618747088,
+          "pitch": 0.07483026291028416,
+          "rotation": 0,
+          "target": "1-arena-gliwice_-aktualne_scena-1effectsresult"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9687577647626107,
+          "pitch": -0.0030177315445669706,
+          "title": "Garaz",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-arena-gliwice_-aktualne_scena-1effectsresult",
+      "name": "arena gliwice_ aktualne_Scena 1.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9653645639054975,
+          "pitch": 0.324004635408766,
+          "rotation": 0.7853981633974483,
+          "target": "0-21"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
